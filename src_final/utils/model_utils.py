b19780cbc1353df1c16aef7073728575dd2bef9e
yyq90 <yyq9012@gmail.com>
2020-11-27 16:14:25 +0800

    weighted ce

--- src_final/utils/model_utils.py
+++ src_final/utils/model_utils.py
@@ -4,10 +4,11 @@ import os
 import torch
 import torch.nn as nn
 from transformers import BertModel
-
+import numpy as np
 from src_final.utils.loss_utils import FocalLoss
 
 import ipdb
+
 
 class BaseModel(nn.Module):
     def __init__(self,
@@ -56,10 +57,11 @@ class bertForSequenceClassification(BaseModel):
     def __init__(self,
                  bert_dir,
                  dropout_prob=0.1,
+                 weighted=False,
+                 focal=False,
                  ):
         super(bertForSequenceClassification, self).__init__(bert_dir=bert_dir,
-                                               dropout_prob=dropout_prob)
-
+                                                            dropout_prob=dropout_prob)
 
         out_dims = self.bert_config.hidden_size
 
@@ -68,10 +70,23 @@ class bertForSequenceClassification(BaseModel):
         self.classifier_tnews = nn.Linear(out_dims, 15)
         self.dropout = nn.Dropout(0.1)
 
+        if focal:
+            self.loss_fct = FocalLoss(alpha=0.25, gamma=2)
+        else:
+            self.loss_fct = nn.CrossEntropyLoss()
 
-        # self.loss_fct = nn.CrossEntropyLoss()
-        self.loss_fct = FocalLoss()
+        self.weighted = False
+        if weighted:
+            self.weighted = True
+            class_nums = [525, 7975, 808, 11210, 3657, 3896, 3657]
+            cn = [1 / e * min(class_nums) for e in class_nums]
+            weight_CE = torch.from_numpy(np.array(cn))
+            self.loss_oce(weighted=weight_CE)
 
+            class_nums = [1111, 4081, 4976, 3991, 5200, 2107, 4118, 3437, 5955, 3632, 3368, 4851, 257, 2886, 3390]
+            cn = [1 / e * min(class_nums) for e in class_nums]
+            weight_CE = torch.from_numpy(np.array(cn))
+            self.loss_tnews(weighted=weight_CE)
 
         init_blocks = [self.classifier_oce, self.classifier_ocnli, self.classifier_tnews]
 
@@ -92,7 +107,7 @@ class bertForSequenceClassification(BaseModel):
             token_type_ids=token_type_ids
         )
         if task:
-            if not isinstance(task,list):
+            if not isinstance(task, list):
                 assert task in ['oce', 'tnews', 'ocnli']
 
                 # seq_out = bert_outputs[0]
@@ -111,7 +126,14 @@ class bertForSequenceClassification(BaseModel):
                     num_labels = 15
                 outputs = (logits,)
                 if labels is not None:
-                    loss = self.loss_fct(logits.view(-1,num_labels), labels.view(-1))
+                    if self.weighted:
+                        if task == 'oce':
+                            loss = self.loss_oce(logits.view(-1, num_labels), labels.view(-1))
+                        elif task == 'tnews':
+                            loss = self.loss_tnews(logits.view(-1, num_labels), labels.view(-1))
+                        else:
+                            loss = self.loss_fct(logits.view(-1, num_labels), labels.view(-1))
+
                     outputs = (loss,) + outputs
 
                 return outputs
@@ -124,9 +146,9 @@ class bertForSequenceClassification(BaseModel):
                 pooled_out = self.dropout(pooled_out)
                 num_labels = []
                 logits = []
-                for idx,task in enumerate(tasks):
+                for idx, task in enumerate(tasks):
                     row = pooled_out[idx]
-                    logit,num_label = None,None
+                    logit, num_label = None, None
                     if task == 'oce':
                         logit = self.classifier_oce(row)
                         num_label = 7
@@ -142,15 +164,22 @@ class bertForSequenceClassification(BaseModel):
 
                 if labels is not None:
                     loss_l = []
-                    for idx,label in enumerate(labels):
-                        loss_l.append(self.loss_fct(logits[idx].view(-1, num_labels[idx]), label.view(-1)))
+                    for idx, label in enumerate(labels):
+                        if self.weighted:
+                            if tasks[idx] == 'oce':
+                                loss_ = self.loss_oce(logits[idx].view(-1, num_labels[idx]), label.view(-1))
+                            elif tasks[idx] == 'tnews':
+                                loss_ = self.loss_tnews(logits[idx].view(-1, num_labels[idx]), label.view(-1))
+                            else:
+                                loss_ = self.loss_fct(logits[idx].view(-1, num_labels[idx]), label.view(-1))
+                            loss_l.append(loss_)
+                        else:
+                            loss_l.append(self.loss_fct(logits[idx].view(-1, num_labels[idx]), label.view(-1)))
+
                     loss = torch.mean(torch.stack(loss_l))
                     outputs = (loss,) + outputs
 
                 return outputs
-
-
-
 
 
 class GateOut(bertForSequenceClassification):
@@ -159,10 +188,10 @@ class GateOut(bertForSequenceClassification):
                  dropout_prob=0.1,
                  ):
         super(GateOut, self).__init__(bert_dir=bert_dir,
-                                               dropout_prob=dropout_prob)
-        self.gate1 = nn.Linear(self.bert_config.hidden_size,self.bert_config.hidden_size)
-        self.gate2 = nn.Linear(self.bert_config.hidden_size,self.bert_config.hidden_size)
-        self.gate3 = nn.Linear(self.bert_config.hidden_size,self.bert_config.hidden_size)
+                                      dropout_prob=dropout_prob)
+        self.gate1 = nn.Linear(self.bert_config.hidden_size, self.bert_config.hidden_size)
+        self.gate2 = nn.Linear(self.bert_config.hidden_size, self.bert_config.hidden_size)
+        self.gate3 = nn.Linear(self.bert_config.hidden_size, self.bert_config.hidden_size)
 
         init_gates = [self.gate1, self.gate2, self.gate3]
 
